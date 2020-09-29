472b47464c8fa5903b890fb8f396a85cb579085c
Yuzuru <yuzuru@YuzurunoAir.AirPort>
2020-09-29 12:13:56 +0900

    Finish chapter4

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,13 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name = "吉田　譲";
+		System.out.println(name);
+
+		name = "サッカー";
+		System.out.println(name);
+
+		final int LUCKY_NUMBER = 7;
+		System.out.println(LUCKY_NUMBER);
+	}
+}
