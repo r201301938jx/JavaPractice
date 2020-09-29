9bd93b554fbd8cc6092d6bff3e2ca47ce6e875b6
Yuzuru <yuzuru@YuzurunoAir.AirPort>
2020-09-29 15:27:31 +0900

    Finish chapter5

---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,26 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long l = 1000L;
+		System.out.println(l);
+
+		double d = 123.456;
+		System.out.println(d);
+
+		boolean b = false;
+		System.out.println(b);
+
+		Float f = 1.234f;
+		System.out.println(f);
+
+		Integer i = 1000;
+		System.out.println(i);
+
+		Character c = 'あ';
+		System.out.println(c);
+
+		int[] array5 = { 10, 20, 30, 40, 50 };
+		System.out.println(array5[3]);
+
+	}
+}
